5aab3a19e2ccfec2a294d2eb804039ad21c1ab33
ChuckNorr1s <117844734+ChuckNorr1s@users.noreply.github.com>
2023-05-23 21:30:04 +0000

    Update subfinder.sh

--- subfinder.sh
+++ subfinder.sh
@@ -10,3 +10,18 @@ if [ -z "$domain" ]; then
 fi
 
 assetfinder --subs-only "$domain" | sort | uniq | tee -a output.txt && cat output.txt | httprobe cat output.txt | tee -a works.txt
+
+echo "...Organizing in a pretty format..."
+
+file="works.txt"
+
+if [ ! -f "$file" ]; then
+  echo "Error: $file does not exist."
+  exit 1
+fi
+
+grep -E '^https://' "$file" >> pretty.txt
+echo "---------------------------" >> pretty.txt
+grep -E '^http://' "$file" >> pretty.txt
+
+echo "...Organized to pretty.txt..."
