3b81b2d3a16e8e022b94e15de5d93c0a20eaf2b5
ChuckNorr1s <117844734+ChuckNorr1s@users.noreply.github.com>
2023-05-24 22:33:55 +0000

    Update and rename subfinder.sh to subfind.sh

--- subfind.sh
+++ subfind.sh
@@ -5,7 +5,7 @@ file1="$2"
 
 if [ -z "$domain" ] || [ -z "$2" ] ; then  
   echo -e "Error: Domain or file arguments are missing.\n"
-  echo "Usage: subfinder.sh <domain> <file-to-save-to>"
+  echo "Usage: subfind <domain> <file-to-save-to>"
   echo "Example: subfinder.sh example.com domains.txt"
   exit 1
 fi
